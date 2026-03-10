764067ae6b94b82a3cabcf0eb1dcfb034908e095
kihaas <qwkihaas@gmail.com>
2026-03-10 20:11:51 +0400

    feat: databases connection

--- core/config.py
+++ core/config.py
@@ -1,7 +1,9 @@
-import os
+from pathlib import Path
 from os import getenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 class Settings(BaseSettings):
--- core/models/product.py
+++ core/models/product.py
@@ -6,7 +6,9 @@ from .base import Base
 class Product(Base):
     __tablename__ = "products"
 
-    name: Mapped[str]  # mapped чтобы скать что это колонки
+    name: Mapped[str]  # mapped чтобы скaзать что это колонки
     description: Mapped[str]
     price: Mapped[int]
-#.
+
+
+# .
