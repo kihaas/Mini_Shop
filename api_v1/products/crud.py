bbcf8759acf93e24b4021fd43b9e6c83d507abf5
kihaas <qwkihaas@gmail.com>
2026-03-11 20:29:21 +0400

    feat: crud

--- api_v1/products/crud.py
+++ api_v1/products/crud.py
@@ -2,12 +2,30 @@
 # read
 # update
 # delete
-
+from sqlalchemy import select
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import Product
 
+from .schemas import ProductCreate
+
 
 async def get_products(session: AsyncSession) -> list[Product]:
-    products = []
-    return products
+    stmt = select(Product).order_by(Product.id)
+    result: Result = await session.execute(stmt)
+    products = result.scalars().all()  # короче scalars надо
+    return list(products)
+
+
+async def get_product(session: AsyncSession, product_id: int) -> Product | None:
+    return await session.get(Product, product_id)
+
+
+async def create_product(session: AsyncSession, product_in: ProductCreate) -> Product:
+    product = Product(
+        **product_in.model_dump()
+    )  # kwagrs model_dump - преобразование в словарь
+    session.add(product)
+    await session.commit()
+    await session.refresh(product)
+    return product
