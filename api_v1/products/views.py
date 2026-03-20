cd6ca85585fe27ba54204be6c3048e9e2bb0f64e
kihaas <qwkihaas@gmail.com>
2026-03-20 22:19:16 +0400

    DEBUGGIN: views

--- api_v1/products/views.py
+++ api_v1/products/views.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper, product
 from . import crud
-from .schemas import Product, ProductCreate, ProductUpdate
+from .schemas import Product, ProductCreate, ProductUpdate, ProductUpdatePartial
 from .dependencies import product_by_id
 
 router = APIRouter(tags=["products"])
@@ -19,7 +19,7 @@ async def get_products(
     return await crud.get_products(session=session)
 
 
-@router.post("/", response_model=Product)
+@router.post("/", response_model=Product, status_code=status.HTTP_201_CREATED)
 async def create_product(
     product_in: ProductCreate,
     session: AsyncSession = Depends(db_helper.session_dependency),
@@ -44,3 +44,23 @@ async def update_product(
         product=product,
         product_update=product_update,
     )
+
+@router.patch("/{product_id}")
+async def update_product_partial(
+        product_update: ProductUpdatePartial,
+        product: Product = Depends(product_by_id),
+        session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    return await crud.update_product(
+        session=session,
+        product=product,
+        product_update=product_update,
+        partial=True,
+    )
+
+router.delete("/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_product(
+        product: Product = Depends(product_by_id),
+        session: AsyncSession = Depends(db_helper.session_dependency),
+) -> None:
+    await crud.delete_product(session=session, product=product)
